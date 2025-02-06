85e6abb5e6ac8bacd4f4c7728ce998604e02e653
Jeff-Schleibaum <121133241+Jeff-Schleibaum@users.noreply.github.com>
2025-02-06 11:53:56 -0500

    Add files via upload
    Automation program to organize files

--- main.py
+++ main.py
@@ -1,86 +1,34 @@
-import sys
-from encryption_utilities import loadPasswordFile, savePasswordFile, passwordEncrypt
+# Jeff Schleibaum automation Project
+"""
+The program defines file categories based on their extensions (e.g., images, documents, videos).
+It scans a folder and retrieves a list of files while ignoring subfolders.
+It handles errors if the folder does not exist.
+"""
 
-#The password list - We start with it populated for testing purposes
-passwords = [["yahoo","XqffoZeo"],["google","CoIushujSetu"]]
+import os
+import shutil
 
-#The password file name to store the passwords to
-passwordFileName = "samplePasswordFile"
-
-#The encryption key for the caesar cypher
-encryptionKey = 16
-
-while True:
-    print("What would you like to do:")
-    print(" 1. Open password file")
-    print(" 2. Lookup a password")
-    print(" 3. Add a password")
-    print(" 4. Save password file")
-    print(" 5. Print the encrypted password list (for testing)")
-    print(" 6. Quit program")
-    print("Please enter a number (1-4)")
-    choice = input()
-
-    if(choice == '1'): #Load the password list from a file
-        passwords = loadPasswordFile(passwordFileName)
-
-    if(choice == '2'): #Lookup at password
-        print("Which website do you want to lookup the password for?")
-        for keyvalue in passwords:
-            print(keyvalue[0])
-        passwordToLookup = input()
-
-        ####### YOUR CODE HERE ######
-        #You will need to find the password that matches the website
-        found = False
-
-        for i in range(len(passwords)): # for loop
-            if passwords[i][0] == passwordToLookup: # checking list with 2 seperate stored items
-                found = True
-                encryptedPassword = passwords[i][1] # creating encrption
-                decryptedPasword = passwordEncrypt(encryptedPassword, -encryptionKey) # decrpting password
-                print(f"The password for {passwordToLookup} is {decryptedPasword} ")  # displaying the website and the decrypted password to the list
-                break
-
-        if not found:
-            print (f"The password for {passwordToLookup} can not be found.") # default messege
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx", ".pptx"],
+    "videos": [",mp4", ".mov", ".avi", ".mkv"],
+    "Audio": [".mp3", ".wav", ".aac"],
+    "Archives": [".zip", ".rar", ".tar", ".gz"],
+    "Others": []
+}
 
 
-    if(choice == '3'):
-        print("What website is this password for?") # prompt for website
-        website = input() # save input
-        print("What is the password?") # prompt for password
-        unencryptedPassword = input() # save input
-
-        ####### YOUR CODE HERE ######
-        #You will need to encrypt the password and store it in the list of passwords
-
-        #The encryption function is already written for you
-        #Step 1: You can say encryptedPassword = passwordEncrypt(unencryptedPassword,encryptionKey)]
-        #the encryptionKey variable is defined already as 16, don't change this
-        #Step 2: create a list of size 2, first item the website name and the second item the password.
-        #Step 3: append the list from Step 2 to the password list
-
-        # step 1
-        encryptedPassword = passwordEncrypt(unencryptedPassword, encryptionKey)
-        # step 2
-        newEntry = [website, encryptedPassword]
-        #step 3
-        passwords.append(newEntry)
-
-        print(f"The password for {website} added successfully.")
-        ####### YOUR CODE HERE ######
-
-    if(choice == '4'): #Save the passwords to a file
-            savePasswordFile(passwords,passwordFileName)
+def get_files(folder_path):
+    """Returns a list of files in the given folder."""
+    try:
+        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        return files
+    except FileNotFoundError:
+        print("Error: The folder does not exist.")
+        return []
 
 
-    if(choice == '5'): #print out the password list
-        for keyvalue in passwords:
-            print(', '.join(keyvalue))
-
-    if(choice == '6'):  #quit our program
-        sys.exit()
-
-    print()
-    print()
+if __name__ == "__main__":
+    folder = input("Enter the path of the folder to organize: ")
+    files = get_files(folder)
+    print("Files found:", files)
